fcc695a66e85ca8704b84b3ae5eba8d5ce5a8705
Tanya Kravchenko <mail-tanja@mail.ru>
2021-05-06 21:24:42 +0300

    Add React.createRef in MyPosts. Learn to add text by clicking on the button.

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -12,6 +12,10 @@ const Dialogs: React.FC<DialogsType> = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem key={d.id} name={d.name} id={d.id} avatar={d.avatar}/>)
     let messagesElements = props.state.messages.map(m => <Message key={m.id} id={m.id} message={m.message}/>)
 
+    let addMessage = () => {
+        alert('Yo')
+    }
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -19,6 +23,12 @@ const Dialogs: React.FC<DialogsType> = (props) => {
             </div>
             <div className={classes.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Add post</button>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -10,15 +10,22 @@ export type MyPostsType = {
 const MyPosts: React.FC<MyPostsType> = (props ) => {
     let postsElement = props.posts.map(post => <Post key={post.id} id={post.id} message={post.message} likes={post.likes}/>)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    let addPost = () => {
+        let text = newPostElement.current?.value;
+        alert(text)
+    }
+
     return (
         <div className={classes.postBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
