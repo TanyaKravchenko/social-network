af5ab1589bb183ed888ef251f446f8144d09c4be
Tanya Kravchenko <mail-tanja@mail.ru>
2021-05-07 17:05:32 +0300

    Refactoring Navbar.

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,6 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-test commit
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
