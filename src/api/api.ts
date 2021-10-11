97ed98d5244f735c71077e9939a54ed800403415
Tanya Kravchenko <mail-tanja@mail.ru>
2021-10-11 23:16:34 +0300

    refactored api, profile-api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {PhotosType, ProfileType, UserType} from '../types/types';
+import {UserType} from '../types/types';
 
 export const instance = axios.create({
     withCredentials: true,
@@ -8,33 +8,6 @@ export const instance = axios.create({
     },
     baseURL: 'https://social-network.samuraijs.com/api/1.0/'
 })
-
-export const profileAPI = {
-    getProfile(userId: number) {
-        return instance.get(`profile/` + userId)
-    },
-    getStatus(userId: number) {
-        return instance.get(`profile/status/` + userId)
-    },
-    updateStatus(status: string) {
-        return instance.put(`profile/status/`, {status: status})
-    },
-    savePhoto(photoFile: File) {
-        const formData = new FormData();
-        formData.append('image', photoFile);
-
-        return instance.put<APIResponseType<SavePhotoResponseDataType>>(`profile/photo`, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        }).then(res => res.data);
-    },
-    saveProfile(profile: ProfileType) {
-        return instance.put<APIResponseType>(`profile`, profile).then(res => res.data);
-    }
-}
-
-
 
 export enum ResultCodesEnum {
     Success = 0,
@@ -56,7 +29,4 @@ export type APIResponseType<D = {}, RC = ResultCodesEnum> = {
     resultCode: RC
 }
 
-type SavePhotoResponseDataType = {
-    photos: PhotosType
-}
 
